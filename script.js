49ad33078cc795caee07409014b4a3d14d0322da
Abegail Pates <abegail.pates@gmail.com>
2022-08-06 20:41:38 +0800

    Write function that plays one single round

--- script.js
+++ script.js
@@ -3,12 +3,52 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+// Create a function that will play one single round
+function playRound(playerSelection, computerSelection) {
+    let whatPlayerSelected = playerSelection.toLowerCase();
+
+    if (whatPlayerSelected === computerSelection) {
+        return "It's a draw!";
+    }
+
+    if (whatPlayerSelected === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock.";
+        } else {
+            return "You win! Rock beats scissors."
+        }
+    }
+
+    if (whatPlayerSelected === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock.";
+        } else {
+            return "You lose! Scissors beat paper.";
+        }
+    }
+
+    if(whatPlayerSelected === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats scissors";
+        } else {
+            return "You win! Paper beats rock."
+        }
+    }
+    
+  
+}
+
+const playerSelection = "PAPER";
+const computerSelection = getComputerChoice();
+
+console.log(`Player selection is: ${playerSelection}`);
+console.log(`Computer selection is: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
